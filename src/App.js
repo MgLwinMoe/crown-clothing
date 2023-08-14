18f178731379a5e4864266800d325cde1653a945
MgLwinMoe <lwinmoe1998kpg@gmail.com>
2023-08-14 15:14:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
+import Directory from './components/directory/directory'
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ]
   return (
-    <div className="categories-container">
-      <div className="category-container">
-          {/* img */}
-          <div className="category-body">
-              <h1>Hats</h1>
-              <p>Shop Now</p>
-          </div>
-      </div>
-      <div className="category-container">
-          {/* img */}
-          <div className="category-body">
-              <h1>Jackets</h1>
-              <p>Shop Now</p>
-          </div>
-      </div>
-      <div className="category-container">
-          {/* img */}
-          <div className="category-body">
-              <h1>Sneakers</h1>
-              <p>Shop Now</p>
-          </div>
-      </div>
-      <div className="category-container">
-          {/* img */}
-          <div className="category-body">
-              <h1>Womens</h1>
-              <p>Shop Now</p>
-          </div>
-      </div>
-      <div className="category-container">
-          {/* img */}
-          <div className="category-body">
-              <h1>Men</h1>
-              <p>Shop Now</p>
-          </div>
-      </div>
-    </div>
+    <Directory categories={categories} />
   )
 }
 export default App
---
+++ src/components/directory/directory.jsx
@@ -0,0 +1,18 @@
+import CategoryItem from '../category-item/CategoryItem'
+import './directory-style.scss';
+
+const Directory = ({categories}) => {
+    return (
+    <div className="directory-container">
+        {
+            categories.map((category) => {
+            return (
+                <CategoryItem category={category} />
+            )
+            })
+        }
+    </div>
+    );
+}
+
+export default Directory
